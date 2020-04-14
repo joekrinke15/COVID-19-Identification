78852d782ede672d2396d456c21badf0b6209a70
abdaloth <abdullahoalothman@gmail.com>
2020-04-14 15:49:13 -0400

    move dir names to global variable

--- model.py
+++ model.py
@@ -28,7 +28,7 @@ BATCH_SIZE = 32
 EPOCHS = 50
 LEARNING_RATE = 1e-3
 LR_DECAY = LEARNING_RATE/EPOCHS
-
+DATA_SRC = 'data/single_src_no_dup' # source folder for the data
 # load base model and freeze learning
 base_model = VGG19(weights="imagenet", include_top=False,
                          input_tensor=nn.Input(shape=INPUT_SHAPE))
@@ -58,16 +58,12 @@ FLOW_PARAMS = {'target_size':INPUT_SHAPE[:2],
         'batch_size':BATCH_SIZE,
         'class_mode':'categorical'}
 
-train_flow = train_gen.flow_from_directory('data/single_src_no_dup/train', **FLOW_PARAMS)
-val_flow = val_gen.flow_from_directory('data/single_src_no_dup/val', **FLOW_PARAMS, shuffle=False)
-test_flow = val_gen.flow_from_directory('data/single_src_no_dup/test', **FLOW_PARAMS, shuffle=False)
+train_flow = train_gen.flow_from_directory(f'{DATA_SRC}/train', **FLOW_PARAMS)
+val_flow = val_gen.flow_from_directory(f'{DATA_SRC}/val', **FLOW_PARAMS, shuffle=False)
+test_flow = val_gen.flow_from_directory(f'{DATA_SRC}/test', **FLOW_PARAMS, shuffle=False)
 
 
 # callback functions
-class_weights = class_weight.compute_class_weight('balanced', 
-                                                np.unique(train_flow.classes),
-                                                train_flow.classes)
-class_weights = class_weights/np.max(class_weights)
 model_name = 'vgg19_single_src_no_dup'
 es_c = EarlyStopping(monitor='val_loss', patience=2, mode='min')
 mc_c = ModelCheckpoint(f'serialized/{model_name}.h5',
@@ -85,10 +81,13 @@ history = model.fit_generator(train_flow,
                             validation_data=val_flow,
                             validation_steps=len(val_flow))
 
+# evaluate model
 model = load_model(f'serialized/{model_name}.h5')
 test_flow.reset()
 predictions = model.predict_generator(test_flow, len(test_flow), verbose=1)
 
 y_true = test_flow.classes
-print(classification_report(y_true, np.argmax(predictions, 1), target_names=test_flow.class_indices))
+print(classification_report(y_true, 
+                            np.argmax(predictions, 1), 
+                            target_names=test_flow.class_indices))
 print(confusion_matrix(y_true, np.argmax(predictions, 1)))
